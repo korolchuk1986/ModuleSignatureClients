4a1846acd3b40216fe816c0319c4a71eac406267
Carine07 <carine.shopping@gmail.com>
2019-03-04 21:56:44 +0100

    La dernière version de la BDD (désolée Vladimir)

--- doc/signature_clients.sql
+++ doc/signature_clients.sql
@@ -39,6 +39,20 @@ INSERT INTO `type_marital` VALUES
 (4, 'veuf\(ve\)'), 
 (5, 'séparé\(e\) de corps');
 
+CREATE TABLE `type_evenement` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `evenement` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+INSERT INTO `type_evenement` VALUES 
+(1,'Mariage'),
+(2,'Divorce'), 
+(3, 'Veuvage'), 
+(4, 'PACS'), 
+(5, 'Rupture de PACS'), 
+(6, 'Séparation de corps');
+
 CREATE TABLE `lieu_mariage` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `lieu` varchar(200) NOT NULL,
@@ -100,6 +114,17 @@ INSERT INTO `divorce_rendu_par` VALUES
 (1,'tribunal de grande instance'), 
 (2,'cour d\'appel');
 
+CREATE TABLE `civilite` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `civilite` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+INSERT INTO `civilite` VALUES 
+(1,'M'), 
+(2,'Mme'), 
+(3,'Mlle');
+
 CREATE TABLE `capacite` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `capacite` varchar(200) NOT NULL,
@@ -115,7 +140,7 @@ INSERT INTO `capacite` VALUES
 
 CREATE TABLE `statut` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `status` varchar(100) NOT NULL,
+  `statut` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
@@ -124,8 +149,115 @@ INSERT INTO `statut` VALUES
 (2,'résident UE'), 
 (3,'non résident');
 
-CREATE TABLE `coordonnees` (
+CREATE TABLE `document` (
+  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `libelle` varchar(200) NOT NULL,
+  `categorie` varchar(200) DEFAULT NULL,
+  `type_doc` varchar(200) NOT NULL,
+  `date_enregistrement` datetime NOT NULL,
+  `lien_vers_contenu` varchar(200) NOT NULL,
+  `id_client` bigint(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `deces` (
+  `id_personne` bigint(20) NOT NULL,
+  `date` date NOT NULL,
+  `ville_etrangere` varchar(200) DEFAULT NULL,
+  `commentaire` varchar(200) DEFAULT NULL,
+  `id_ville` bigint(20) DEFAULT NULL,
+  `id_pays` bigint(20) NOT NULL,
+  PRIMARY KEY (`id_personne`), 
+  CONSTRAINT `fk_deces_id_pays` FOREIGN KEY (`id_pays`) REFERENCES `pays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION, 
+  CONSTRAINT `fk_deces_id_ville` FOREIGN KEY (`id_ville`) REFERENCES `ville` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `deces` VALUES 
+(1,'2019-02-21', null, 'décès à l\'hôpital', 1, 1);
+
+CREATE TABLE `adresse` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `est_principale` boolean NOT NULL,
+  `num` int(4) DEFAULT NULL,
+  `ordre_voie` varchar(5) DEFAULT NULL,
+  `type_voie` varchar(15) DEFAULT NULL,
+  `nom_voie` varchar(200) NOT NULL,
+  `complement1` varchar(200) NOT NULL,
+  `complement2` varchar(200) NOT NULL,
+  `lieu_dit` varchar(200) NOT NULL,
+  `spf` varchar(200) NOT NULL,
+  `ville_etrangere` varchar(200) DEFAULT NULL,
+  `id_ville` bigint(20) DEFAULT NULL,
+  `id_pays` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_personne_id_ville` FOREIGN KEY (`id_ville`) REFERENCES `ville` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION, 
+  CONSTRAINT `fk_personne_id_pays` FOREIGN KEY (`id_pays`) REFERENCES `pays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+INSERT INTO `adresse` VALUES 
+(1, true, 4, 'bis', 'rue', 'Louis XIV', null, null, null, null, null, 1, 1),
+(2, true, 5, 'ter', 'avenue', 'Napoléon', null, null, null, null, null, 2, 1);
+
+CREATE TABLE `historique` (
+  `id_client` bigint(20) NOT NULL,
+  `id_evenement` bigint(20) NOT NULL,
+  PRIMARY KEY (`id_client`, `id_evenement`),
+  KEY `fk_historique_id_client_idx` (`id_client`),
+  KEY `fk_historique_id_evenement_idx` (`id_evenement`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `historique` 
+VALUES 
+(1, 1),
+(2, 2);
+
+CREATE TABLE `evenement` (
+  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id_type_evenement` bigint(20) DEFAULT NULL,
+  `id_type_PACS` bigint(20) default null, 
+  `date` date DEFAULT NULL,
+  `jugement_divorce_date` date DEFAULT NULL,
+  `comment_separation` varchar(200) DEFAULT NULL,
+  `ville_etrangere_mariage` varchar(200) DEFAULT NULL,
+  `id_cause_rupture_PACS` bigint(20) DEFAULT NULL,
+  `notaire` varchar(50) DEFAULT NULL,
+  `id_divorce_rendu_par` bigint(20) DEFAULT NULL,
+  `id_lieu_mariage` bigint(20) DEFAULT NULL,
+  `id_regime_mariage` bigint(20) DEFAULT NULL,
+  `id_pays_mariage` bigint(20) DEFAULT NULL,
+  `id_ville` bigint(20) DEFAULT NULL,
+  `id_conjoint` bigint(20) DEFAULT NULL,
+  CONSTRAINT `fk_evenement_id_type_evenement` FOREIGN KEY (`id_type_evenement`) REFERENCES `type_evenement` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_evenement_id_type_PACS` FOREIGN KEY (`id_type_PACS`) REFERENCES `type_PACS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_evenement_id_divorce_rendu_par` FOREIGN KEY (`id_divorce_rendu_par`) REFERENCES `divorce_rendu_par` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION, 
+  CONSTRAINT `fk_evenement_id_lieu_mariage` FOREIGN KEY (`id_lieu_mariage`) REFERENCES `lieu_mariage` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_evenement_id_regime_mariage` FOREIGN KEY (`id_regime_mariage`) REFERENCES `regime_mariage` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_evenement_id_pays_mariage` FOREIGN KEY (`id_pays_mariage`) REFERENCES `pays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_evenement_id_ville` FOREIGN KEY (`id_ville`) REFERENCES `ville` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+INSERT INTO `evenement` (`id`, `id_type_evenement`, `id_conjoint`)
+VALUES 
+(1, 1, null),
+(2, 2, 3);
+
+CREATE TABLE `personne` (
+  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `est_client` boolean NOT NULL,
+  `id_civilite` bigint(20) DEFAULT NULL,
+  `nom` varchar(200) NOT NULL,
+  `prenoms` varchar(200) DEFAULT NULL,
+  `date_naissance` date DEFAULT NULL,
+  `ville_etrangere_naissance` varchar(50) DEFAULT NULL,
+  `nationalite` varchar(50) DEFAULT NULL,
+  `profession` varchar(200) DEFAULT NULL,
+  `nom_usuel` varchar(200) DEFAULT NULL,
+  `prenom_usuel` varchar(200) DEFAULT NULL,
+  `id_type_marital` bigint(20) NOT NULL,
+  `est_pacse` boolean NOT NULL,
+  `id_conjoint` bigint(20) DEFAULT NULL,
+  `clerc_referent` varchar(200) DEFAULT NULL,
+  `notaire_referent` varchar(200) DEFAULT NULL,
+  `date_modif_fiche` date DEFAULT NULL,
   `telephone` varchar(200) NOT NULL,
   `comment_telephone` varchar(200) DEFAULT NULL,
   `telephone_pro` varchar(200) DEFAULT NULL,
@@ -138,122 +270,13 @@ CREATE TABLE `coordonnees` (
   `comment_fax` varchar(200) DEFAULT NULL,
   `site_web` varchar(200) DEFAULT NULL,
   `comment_site_web` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-INSERT INTO `coordonnees` (`id`, `telephone`)
-VALUES 
-(1,'0478005634'),
-(2,'0176983209');
-
-CREATE TABLE `document` (
-  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `libelle` varchar(200) NOT NULL,
-  `categorie` varchar(200) DEFAULT NULL,
-  `typeDoc` varchar(200) NOT NULL,
-  `dateEnregistrement` datetime NOT NULL,
-  `document` blob NOT NULL,
-  `id_client` bigint(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `deces` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `date` date NOT NULL,
-  `commentaire` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-INSERT INTO `deces` VALUES 
-(1,'2019-02-21','décès à l\'hôpital');
-
-CREATE TABLE `adresse` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `num` int(4) DEFAULT NULL,
-  `ordre_voie` varchar(5) DEFAULT NULL,
-  `type_voie` varchar(15) DEFAULT NULL,
-  `nom_voie` varchar(200) NOT NULL,
-  `id_personne` bigint(20) NOT NULL,
-  `id_ville` bigint(20) NOT NULL,
-  `id_pays` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_deces_id_personne_idx` (`id_personne`), 
-  CONSTRAINT `fk_personne_id_ville` FOREIGN KEY (`id_ville`) REFERENCES `ville` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_personne_id_pays` FOREIGN KEY (`id_pays`) REFERENCES `pays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-INSERT INTO `adresse` VALUES 
-(1, 4, 'bis', 'rue', 'Louis XIV', 1, 1, 1),
-(2, 5, 'ter', 'avenue', 'Napoléon', 1, 1, 2);
-
-CREATE TABLE `evenement` (
-  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `id_type_marital` bigint(20) NOT NULL,
-  `est_pacse` boolean NOT NULL,
-  `id_type_PACS` bigint(20) default null, 
-  `date` date DEFAULT NULL,
-  `jugement_divorce_date` date DEFAULT NULL,
-  `comment_separation` varchar(200) DEFAULT NULL,
-  `id_cause_rupture_PACS` bigint(20) DEFAULT NULL,
-  `notaire` varchar(50) DEFAULT NULL,
-  `id_divorce_rendu_par` bigint(20) DEFAULT NULL,
-  `id_lieu_mariage` bigint(20) DEFAULT NULL,
-  `id_regime_mariage` bigint(20) DEFAULT NULL,
-  `id_pays_mariage` bigint(20) DEFAULT NULL,
-  `id_ville` bigint(20) DEFAULT NULL,
-  `id_client` bigint(20) NOT NULL,
-  `id_conjoint` bigint(20) DEFAULT NULL,
-  KEY `fk_evenement_id_client_idx` (`id_client`), 
-  CONSTRAINT `fk_evenement_id_type_marital` FOREIGN KEY (`id_type_marital`) REFERENCES `type_marital` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_evenement_id_type_PACS` FOREIGN KEY (`id_type_PACS`) REFERENCES `type_PACS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_evenement_id_divorce_rendu_par` FOREIGN KEY (`id_divorce_rendu_par`) REFERENCES `divorce_rendu_par` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION, 
-  CONSTRAINT `fk_evenement_id_lieu_mariage` FOREIGN KEY (`id_lieu_mariage`) REFERENCES `lieu_mariage` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_evenement_id_regime_mariage` FOREIGN KEY (`id_regime_mariage`) REFERENCES `regime_mariage` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_evenement_id_pays_mariage` FOREIGN KEY (`id_pays_mariage`) REFERENCES `pays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_evenement_id_ville` FOREIGN KEY (`id_ville`) REFERENCES `ville` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-INSERT INTO `evenement` (`id`, `id_type_marital`, `est_pacse`, `id_client`, `id_conjoint`)
-VALUES 
-(1, 1, false, 1, null),
-(2, 2, false, 2, 3);
-
-CREATE TABLE `historique` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `id_client` bigint(20) NOT NULL,
-  `id_evenement` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_historique_id_client_idx` (`id_client`),
-  KEY `fk_historique_id_evenement_idx` (`id_evenement`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-INSERT INTO `historique` 
-VALUES 
-(1, 1, 1),
-(2, 2, 2);
-
-CREATE TABLE `personne` (
-  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `est_client` boolean NOT NULL,
-  `civilite` varchar(10) DEFAULT NULL,
-  `nom` varchar(200) NOT NULL,
-  `prenoms` varchar(200) DEFAULT NULL,
-  `date_naissance` date DEFAULT NULL,
-  `nationalite` varchar(50) DEFAULT NULL,
-  `profession` varchar(200) DEFAULT NULL,
-  `nom_usuel` varchar(200) DEFAULT NULL,
-  `prenom_usuel` varchar(200) DEFAULT NULL,
-  `id_etat_marital_PACS` bigint(20) DEFAULT NULL,
-  `clerc_referent` varchar(200) DEFAULT NULL,
-  `notaire_referent` varchar(200) DEFAULT NULL,
-  `date_modif_fiche` date DEFAULT NULL,
-  `id_coordonnes` bigint(20) DEFAULT NULL,
   `id_statut` bigint(20) DEFAULT NULL,
   `id_pays_naissance` bigint(20) DEFAULT NULL,
   `id_ville_naissance` bigint(20) DEFAULT NULL,
   `id_capacite` bigint(20) DEFAULT NULL,
-  `id_deces` bigint(20) DEFAULT NULL,
-  `id_coordonnees` bigint(20) DEFAULT NULL,
-  CONSTRAINT `fk_personne_id_coordonnees` FOREIGN KEY (`id_coordonnees`) REFERENCES `coordonnees` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_personne_id_civilite` FOREIGN KEY (`id_civilite`) REFERENCES `civilite` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_personne_id_type_marital` FOREIGN KEY (`id_type_marital`) REFERENCES `type_marital` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_personne_id_conjoint` FOREIGN KEY (`id_conjoint`) REFERENCES `personne` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_personne_id_statut` FOREIGN KEY (`id_statut`) REFERENCES `statut` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_personne_id_pays_naissance` FOREIGN KEY (`id_pays_naissance`) REFERENCES `pays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_personne_id_ville_naissance` FOREIGN KEY (`id_ville_naissance`) REFERENCES `ville` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
@@ -261,23 +284,24 @@ CREATE TABLE `personne` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 INSERT INTO `personne` 
-(`id`, `est_client`, `nom`, `prenoms`,`id_etat_marital_PACS`)
+(`id`, `est_client`, `nom`, `prenoms`, `id_type_marital`, `est_pacse`, `id_conjoint`, `telephone`, `email`)
 VALUES 
-(1, true, "dupont", "jean", 1),
-(2, true, "martin", "julie", 2), 
-(3, false, "martin", "michel", 2);
+(1, true, "dupont", "jean", 1, false, null, "0102030406", "jean@epsi.fr"),
+(2, true, "martin", "julie", 2, false, null, "0475345657", "julie@epsi.fr"), 
+(3, false, "martin", "michel", 2, false, 2, "0475345657", "michel@epsi.fr");
+
+UPDATE `personne` 
+SET id_conjoint = 3 WHERE id=2; 
 
 ALTER TABLE document
 ADD CONSTRAINT `fk_document_id_client` FOREIGN KEY (`id_client`) REFERENCES `personne` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE adresse
-ADD CONSTRAINT `fk_adresse_id_personne` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
 ALTER TABLE evenement
-ADD CONSTRAINT `fk_evenement_id_client` FOREIGN KEY (`id_client`) REFERENCES `personne` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION, 
 ADD CONSTRAINT `fk_evenement_id_conjoint` FOREIGN KEY (`id_conjoint`) REFERENCES `personne` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 ALTER TABLE historique
 ADD CONSTRAINT `fk_historique_id_client` FOREIGN KEY (`id_client`) REFERENCES `personne` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
 ADD CONSTRAINT `fk_historique_id_evenement` FOREIGN KEY (`id_evenement`) REFERENCES `evenement` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
+ALTER TABLE deces
+ADD CONSTRAINT `fk_deces_id_personne` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id`) ON DELETE cascade ON UPDATE NO ACTION;
--- src/main/java/fr/fiducial/signature/feature/clients/dao/PersonneDAO.java
+++ src/main/java/fr/fiducial/signature/feature/clients/dao/PersonneDAO.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonneDAO extends CrudRepository<Personne, Long> {
-    @Query("select p.civilite, p.nom, p.prenom, p.dateModifFiche, p.id from Personne as p where p.estClient=true")
+    @Query("from Personne as p where p.estClient=true")
     Iterable<Personne> findClients();
 
     Iterable<Personne> findAllByEstClientTrue();
